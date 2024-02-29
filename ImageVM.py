c28405b4de69c3ba974fa2cd0f4791a92b3e98ae
rahidaliyev <rahidaliyev18@gmail.com>
2024-03-01 01:13:05 +0400

    model

---
+++ ImageVM.py
@@ -0,0 +1,8 @@
+class ImageVM:
+    def __init__(self,kind,title,link,displayLink,fileFormat):
+        self.kind = kind
+        self.title = title
+        self.link = link
+        self.displayLink = displayLink
+        self.fileFormat = fileFormat
+        
