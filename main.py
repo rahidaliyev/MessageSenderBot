1d3c125197c6feb7a221c452596414b13666eb35
rahidaliyev <rahidaliyev18@gmail.com>
2024-03-03 05:48:07 +0400

    changed to method

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import pywhatkit as pwk
-try:
-    # pwk.sendwhatmsg_instantly('+994553386385','salam1',1);
-    pwk.sendwhats_image('+994553386385',r"C:\\Users\\rahid\\OneDrive\\Pictures\\Ekran təsvirləri\Screenshot (315).png");
-    print("Message sent!");
-except:
-    print("Error in sending message!")
-    
-
-    
-
-    
-
+def send_message(link):
+    try:
+        pwk.sendwhatmsg_instantly('+994703558057',link,5,tab_close=True);
+        print("Message sent!");
+    except:
+        print("Error in sending message!")
